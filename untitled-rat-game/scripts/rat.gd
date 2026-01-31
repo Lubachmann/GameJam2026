cff8a2eb1b083ebfe8d481df7a32aa682f65e2e3
Luc Bachmann <luc@bachmann-schneider.ch>
2026-01-31 11:16:41 +0100

    new sprite + animation

--- untitled-rat-game/scripts/rat.gd
+++ untitled-rat-game/scripts/rat.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,7 +20,9 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.play("walking")
+		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		animated_sprite.play("idle")
 	move_and_slide()
