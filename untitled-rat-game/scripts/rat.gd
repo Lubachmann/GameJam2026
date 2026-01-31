e7820db610e9054fbebc2fd8bbb6bfa575130c4a
luc <luc@bachmann-schneider.ch>
2026-01-31 14:07:01 +0100

    add mask animation
    movement with "a" and "d"

--- untitled-rat-game/scripts/rat.gd
+++ untitled-rat-game/scripts/rat.gd
@@ -5,6 +5,13 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var mask = $masks
+
+func equip_mask():
+	mask.visible = true
+	
+func unequip_mask():
+	mask.visible = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,6 +21,12 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("up"):
+		equip_mask()
+
+	if Input.is_action_just_pressed("down"):
+		unequip_mask()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,8 +34,12 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 		animated_sprite.play("walking")
+		mask.play("walk")
 		animated_sprite.flip_h = direction < 0
+		mask.flip_h = direction < 0
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite.play("idle")
+		mask.play("idle")
 	move_and_slide()
